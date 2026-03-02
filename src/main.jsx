6d2d9c851cb0f6b602cb1723c6b4b11b71a5a8c6
rajesh827 <rajeshbhandari2075@gmail.com>
2026-03-02 19:36:58 +0545

    update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+
+// import App from "./App1.jsx";
+// import App from "./App2.jsx";
+// import App from "./App3.jsx";
+// import App from "./App4.jsx";
+// import App from "./App5.jsx";
+// import App from "./App6.jsx";
+// import App from "./App7.jsx";
 // import App from "./App8.jsx";
 // import App from "./App9.jsx";
-import App from "./App10.jsx";
+// import App from "./App10.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
